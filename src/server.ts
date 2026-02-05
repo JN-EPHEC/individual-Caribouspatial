d1f5ac043bf499197e2550168c99ee0cf16b8aa2
Caribouspatial <ju.aerts@students.ephec.be>
2026-02-05 15:54:35 +0100

    point 5.2.4 du tp1

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Bienvenue sur mon serveur API");
+});
+
+app.listen(port, () => {
+   console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+
+function greet(name : string): string {
+    return name
+}
+
+console.log(greet("Hello world!"));
