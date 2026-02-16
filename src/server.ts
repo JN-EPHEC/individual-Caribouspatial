46906621390ad43abba313d6c09e1b0295ee383f
Caribouspatial <ju.aerts@students.ephec.be>
2026-02-16 17:38:44 +0100

    Point 6.2 tp1

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,24 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// Interface TypeScript pour typer les étudiants
+interface Etudiant {
+    id: number;
+    nom: string;
+    prenom: string;
+}
+const etudiants: Etudiant[] = [
+    { id: 1, nom: "Dupont", prenom: "Jean" },
+    { id: 2, nom: "Martin", prenom: "Sophie" },
+    { id: 3, nom: "Doe", prenom: "John" }
+];
+//API root
 app.get("/", (req, res) => {
     res.send("Bienvenue sur mon serveur API");
+});
+//API tableau étudinat dans /api/data
+app.get("/api/data", (req, res) => {
+   res.json(etudiants);
 });
 
 app.listen(port, () => {
