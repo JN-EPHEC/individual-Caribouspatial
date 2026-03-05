e63714defcb90a6097fa7cd82bc4a63dbf4f35d0
Caribouspatial <ju.aerts@students.ephec.be>
2026-03-05 14:16:18 +0100

    TP 3 2.5.2 (fini)

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,9 +2,38 @@ import { Router } from 'express';
 import {deleteUser, getAllUsers, postUser} from "../controllers/userController";
 //##########################################################################
 const router = Router();
-//##########################################################################
-router.get('/users', getAllUsers);  // GET Récupérer tous les utilisateurs
-router.post('/users', postUser);    // POST Créer un utilisateur
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *      summary: Récupère la liste des utilisateurs
+ *      tags: [Users, GET]
+ *      responses:
+ *          200:
+ *              description: Succès
+ */
+router.get('/users', getAllUsers);
+/**
+ * @swagger
+ * /api/users:
+ *  post:
+ *      summary: Envoi l'utilisateur que l'on veut ajouter à la DB
+ *      tags: [Users, POST]
+ *      responses:
+ *          200:
+ *              description: Succès
+ */
+router.post('/users', postUser);
+/**
+ * @swagger
+ * /api/users:
+ *  delete:
+ *      summary: Suppression d'un utilisateur par son id
+ *      tags: [Users, DELETE]
+ *      responses:
+ *          200:
+ *              description: Succès
+ */
 router.delete('/users/:id', deleteUser);    // DELETE Supprimer un utilisateur par son ID
 //##########################################################################
 export default router;
